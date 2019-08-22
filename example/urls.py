d83cd00869dc04f58ddcb26ae8536c2f4013e2b8
Jonan Seeley <jonans99@yahoo.com>
2019-08-22 15:30:38 -0400

    more config for heroku

--- example/urls.py
+++ example/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpattern += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
